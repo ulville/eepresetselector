e8e1b104810e69ec875a4448a9e99fe4bfe41177
ulville <50755946+ulville@users.noreply.github.com>
2023-12-07 02:56:27 +0300

    quote arg variable in update-locale.sh

--- update-locale.sh
+++ update-locale.sh
@@ -9,7 +9,7 @@ xgettext -j ./*.js -o "$pot" --from-code UTF-8
 xgettext -j preferences/*.js -o "$pot" --from-code UTF-8
 xgettext -j schemas/*.xml -o "$pot" --from-code UTF-8
 
-if [ $1 = "-a" ]; then
+if [ "$1" = "-a" ]; then
     echo "Template created as $pot"
     echo "Use it to create a po file for a new language"
     echo -e "Don't forget to remove \e[33m$pot\e[0m after you created your .po file"
